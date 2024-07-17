a4b92b47518e31a6e504a1c26b91b9b7c2c18c30
maheshraskargwc <maheshraskargwc@gmail.com>
2024-07-18 02:48:36 +0530

    Adding new Field

---
+++ Models/Ecommerce/order.models.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    order:{
+        type:Number,
+        required:true
+    },
+    Customer :{
+        type:mongoose.Schema.Type.ObjectId,
+        ref:"User"
+    }
+
+},{timestames:true});
+
+export const  Order = mongoose.model("Order",orderSchema);
